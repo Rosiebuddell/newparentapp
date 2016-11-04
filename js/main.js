ed49aa191533c8ed05ad49785f36e1f419714f3d
Rosie Buddell <r.buddell@students.rave.ac.uk>
2016-11-04 20:41:02 +0000

    Initial push

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// use jQuery to select the html elements we're going to manipulate
+//camel case eg goButton is just to make names easier to read because spaces aren't allowed.
+var homeGoButton = $('#home button')
+var homeDropdown = $('#home select')
+var homeSection = $('#home')
+var resultsSection = $('#results')
+var resultsBackButton = $("#results .back")
+var resultsOL = $('#results ol')
+var detailsInfo = $('#details #info')
+var detailsSection = $('#details')
+var detailsBackButton =$('#details .back')
+
+// tell the button to do something when we click it
+//ask matteo wtf the empty brackets are about
+homeGoButton.click( function(){
+    
+    //capture the user chosen option
+    var chosenOption = homeDropdown.val()
+    console.log("You picked " + chosenOption)
+    
+    // filter+sort people by user selection
+    var resultsList = filterAndSortList(productsList, chosenOption);
+    console.log(resultsList);
+    
+    // show the results in the #results section
+    showList(resultsList, resultsOL);
+    
+    $('#results li').click( function() {
+        // grab the id from the clicked item
+        var resultId = $(this).attr('id')
+        // use the id to get the right data
+        var resultData = resultsList[resultId]
+        console.log(resultData)
+        //call the function showDetails()
+        showDetails(resultData, detailsInfo)
+        // show the details!
+        resultsSection.hide()
+        detailsSection.show()
+        })
+            function showDetails (data, interfaceElement) 
+            {
+            var detailsHTML = makeDetailsHTML(data)
+            interfaceElement.html(detailsHTML)
+            }
+    
+            
+    
+            homeSection.hide()
+            resultsSection.show() 
+    
+})
+
+//tell the back button to do something when we click it
+resultsBackButton.click( function(){
+    resultsSection.hide()
+    homeSection.show()
+})
+
+detailsBackButton.click( function(){
+    detailsSection.hide()
+    resultsSection.show()
+})
